97fdea57618905863b602e8641bdfc6a783c18d0
oburu <oburusolo@gmail.com>
2017-03-17 12:06:46 +0000

    basura

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 
-// Create our app
-var app = express();
-const PORT = process.env.PORT || 3000 ;//eviroment variable or heroku/firebase
 
-app.use(function(req, res, next){
-  if(req.headers['x-forwarded-proto'] === 'http'){
+var app = express();
+
+
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
     res.redirect('http://' + req.hostname + req.url);
