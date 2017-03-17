10d0780379e58b48914412bbeddc82ded82a14c6
oburu <oburusolo@gmail.com>
2017-03-17 15:37:22 +0000

    style nav bar

--- app/components/nav.js
+++ app/components/nav.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Link, IndexLink} from 'react-router';
 
-const Nav = props =>{
-  return(
-    <div>
-      <h2>Nav Component</h2>
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      <Link to="/about" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
-      <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-    </div>
-  )
+class Nav extends Component {
+  onSearch(e){
+    e.preventDefault();
+    alert('not yet wired up');
+  }
+
+  render(){
+    return(
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">Weather by React</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li><input type="search" placeholder="Search" /></li>
+              <li><input type="submit" className="button" value="Get Weather" /></li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Nav;
