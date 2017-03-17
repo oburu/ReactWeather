578e7d1ace7258f19599731f41ea70cda37e13a5
oburu <oburusolo@gmail.com>
2017-03-17 15:56:13 +0000

    all centered

--- app/components/examples.js
+++ app/components/examples.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 const Examples = props =>{
   return(
-    <h3> Examples Component </h3>
+    <div>
+      <h1 className="text-center"> Examples</h1>
+      <p>Here ara a few example locations to try out</p>
+      <ol>
+        <li>
+          <Link to='/?location=Manchester'>Manchester, UK</Link>
+        </li>
+        <li>
+          <Link to='/?location=Tarquinia'>Tarquinia, IT</Link>
+        </li>
+      </ol>
+    </div>
+
   )
 }
 
