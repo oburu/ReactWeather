61ca55b59475edccfb159671e76417cedd1d0cf9
oburu <oburusolo@gmail.com>
2017-03-17 12:24:46 +0000

    probando el server

--- server.js
+++ server.js
@@ -6,13 +6,16 @@ var app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
-    res.redirect('http://' + req.hostname + req.url);
-  }
-});
+if(process.env.PORT){
+  app.use(function (req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'http') {
+      next();
+    } else {
+      res.redirect('http://' + req.hostname + req.url);
+    }
+  });
+}
+
 
 app.use(express.static('public'));
 
